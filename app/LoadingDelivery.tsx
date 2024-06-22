d394a7bc454a5be2c5f324976db13d7f93b4020b
MomenMustafa45 <momenkara2@gmail.com>
2024-06-22 10:46:40 +0300

    feat: loading delivery screen added

---
+++ app/LoadingDelivery.tsx
@@ -0,0 +1,28 @@
+import { View, Text, Image } from "react-native";
+import React, { useEffect } from "react";
+import { useNavigation } from "@react-navigation/native";
+import { ScreenNavigationProp } from ".";
+
+type LoadingDeliveryScreenNavigationProp =
+  ScreenNavigationProp<"LoadingDelivery">;
+
+const LoadingDelivery = () => {
+  const navigation = useNavigation<LoadingDeliveryScreenNavigationProp>();
+
+  useEffect(() => {
+    setTimeout(() => {
+      navigation.replace("Delivery");
+    }, 3000);
+  }, []);
+
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Image
+        source={require("@/assets/images/deliveryGuy2.png")}
+        style={{ width: "35%", height: "35%" }}
+      />
+    </View>
+  );
+};
+
+export default LoadingDelivery;
