635ba15018eee025c10e3bf9b12808d501ad3085
MomenMustafa45 <momenkara2@gmail.com>
2024-06-22 10:43:54 +0300

    fix: repeated type fixed and its only in index file only

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,17 @@
 import MyStack from "@/navigation/StackNavigation";
-import { Text, View } from "react-native";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { Restaurant } from "@/components/Features";
+
+type RootStackParamList = {
+  LoadingDelivery: undefined;
+  Delivery: undefined;
+  Home: undefined;
+  Restaurant: { restaurant: Restaurant };
+  Cart: undefined;
+};
+
+export type ScreenNavigationProp<Screen extends keyof RootStackParamList> =
+  NativeStackNavigationProp<RootStackParamList, Screen>;
 
 export default function Index() {
   return (
