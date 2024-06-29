70d8d80b722ef7da1522164eb9f5bea520cbb4ca
MomenMustafa45 <momenkara2@gmail.com>
2024-06-29 16:40:27 +0300

    feat: make color theme to app

--- theme/Theme.ts
+++ theme/Theme.ts
@@ -1,4 +1,22 @@
+const palette = [
+  {
+    text: "#f97316",
+    bgColor: (opacity: number) => `rgba(251,146,60,${opacity})`,
+  },
+  {
+    text: "#fff",
+    bgColor: (opacity: number) => `rgba(2, 48, 71,${opacity})`,
+  },
+  {
+    text: "#000",
+    bgColor: (opacity: number) => `rgba(255, 183, 3,${opacity})`,
+  },
+  {
+    text: "#023047",
+    bgColor: (opacity: number) => `rgba(33, 158, 188,${opacity})`,
+  },
+];
+
 export const ThemeColors = {
-  text: "#f97316",
-  bgColor: (opacity: number) => `rgba(251,146,60,${opacity})`,
+  ...palette[0],
 };
